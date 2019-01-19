9ce58d4934d23427296331106b5a91981a85f920
qwui <qwui301256@sina.com>
2019-01-19 13:54:31 +0800

    添加数据库创建方法

---
+++ 创建数据库/insert.sql
@@ -0,0 +1,7 @@
+
+
+use saihan;
+
+insert into user(type, username, password_hash) values('BUSINESS', 'seller1', "pbkdf2:sha256:50000$AmvDhqDQ$7e68cbb47c8627127b63b53aedd9969a98e5e5385443c07d54feeca5826ff2a8");
+
+insert into user(type, username, password_hash) values('PERSONAL', 'person1', "pbkdf2:sha256:50000$AmvDhqDQ$7e68cbb47c8627127b63b53aedd9969a98e5e5385443c07d54feeca5826ff2a8");
